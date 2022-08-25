b230a82ccd0ce0597620f6418205031b4cd19250
3dharmadev <69399810+3dharmadev@users.noreply.github.com>
2022-08-26 00:22:22 +0530

    small JS part added

---
+++ pricing_page/pricing.js
@@ -0,0 +1,7 @@
+let count=document.getElementById("count");
+let range=document.getElementById("range").value;
+function showData(){
+    let P=document.getElementById("h1");
+         P.innerText=range;
+         console.log(range);
+}
