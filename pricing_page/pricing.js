e294803d0ac18db829c42ecbff5c674973f3bc84
3dharmadev <69399810+3dharmadev@users.noreply.github.com>
2022-08-27 16:36:48 +0530

    pricing page added

--- pricing_page/pricing.js
+++ pricing_page/pricing.js
@@ -1,7 +1,3 @@
-let count=document.getElementById("count");
-let range=document.getElementById("range").value;
-function showData(){
-    let P=document.getElementById("h1");
-         P.innerText=range;
-         console.log(range);
-}
+
+let business_price=document.getElementById("count1"); 
+let Professional_price=document.getElementById("count2");
