fb91b3444dc54b388fc3c5b2db3c254796433dc2
3dharmadev <69399810+3dharmadev@users.noreply.github.com>
2022-08-27 16:38:25 +0530

    final commit

--- pricing_page/pricing.js
+++ pricing_page/pricing.js
@@ -1,3 +1,22 @@
 
 let business_price=document.getElementById("count1"); 
 let Professional_price=document.getElementById("count2");
+
+
+function reload(){
+     
+    let count=document.getElementById("count").value;
+
+     if(Number(count)>200){
+        alert("Team size needs to be less than or equals to 200!");
+        return ;
+    }
+        business_price.innerText=Number(count)*7;
+        Professional_price.innerText=Number(count)*5;
+        if(count==""){
+            business_price.innerText="7";
+            Professional_price.innerText="5";
+        }
+}
+ 
+ 
